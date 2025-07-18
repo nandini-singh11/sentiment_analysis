feadf71c3506cce6a4526298e1afb2499878b40f
nandini-singh11 <147043647+nandini-singh11@users.noreply.github.com>
2025-07-18 14:24:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from textblob import TextBlob
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+    text = data.get('text', '')
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+
+    if polarity > 0:
+        sentiment = "Positive 😊"
+    elif polarity < 0:
+        sentiment = "Negative 😞"
+    else:
+        sentiment = "Neutral 😑"
+
+    return jsonify({'sentiment': sentiment, 'score': polarity})
+
+if __name__ == '__main__':
+    app.run(debug=True)
