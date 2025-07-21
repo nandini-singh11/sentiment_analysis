9a9e2351ee4e5b9bc4f0e3a127ff2ed6b3df6cbf
nandini-singh11 <147043647+nandini-singh11@users.noreply.github.com>
2025-07-21 13:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from textblob import TextBlob
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
 @app.route('/analyze', methods=['POST'])
 def analyze():
     data = request.get_json()
